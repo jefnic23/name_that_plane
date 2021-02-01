144099b647a8c62c5e567e9f06ea07b9ed3b1ca6
Jeff Nicholas <jefnic23@gmail.com>
2021-02-01 02:52:07 -0500

    changed template

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,4 @@ from app import app
 
 @app.route('/main')
 def main():
-    return render_template('main.html', image="0.jpg")
+    return render_template('main.html', image="0.jpg", answer_1="Boeing 737", answer_2="Boeing 737", answer_3="Boeing 737", answer_4="Boeing 737",)
