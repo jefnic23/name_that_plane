264ae84e3d51580fa54a9890ce5a4b47c90cb209
Jeff Nicholas <jefnic23@gmail.com>
2021-02-17 16:03:41 -0500

    fixed path

--- Scripts/Activate.ps1
+++ Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\jefni\Documents\Coding\name_that_plane"
+$env:VIRTUAL_ENV="E:\Coding\spot_the_plane"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
@@ -35,7 +35,7 @@ if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     function global:_OLD_VIRTUAL_PROMPT {""}
     copy-item function:prompt function:_OLD_VIRTUAL_PROMPT
     function global:prompt {
-        Write-Host -NoNewline -ForegroundColor Green '(name_that_plane) '
+        Write-Host -NoNewline -ForegroundColor Green '(spot_the_plane) '
         _OLD_VIRTUAL_PROMPT
     }
 }
--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Coding\name_that_plane
+set VIRTUAL_ENV=E:\Coding\spot_the_plane
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(name_that_plane) %PROMPT%
+set PROMPT=(spot_the_plane) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
