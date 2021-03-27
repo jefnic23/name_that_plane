87a83f8dd7f56430e068270d6739b37d58c0226a
Jeff Nicholas <jefnic23@gmail.com>
2021-03-26 22:34:05 -0400

    changed images

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, jsonify
-import random, time, pandas as pd
+import random, time, glob, os
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,42 +11,20 @@ def index():
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
-    aircraft_list = pd.read_pickle('E:/Coding/spot_the_plane/app/static/dicts/otherModels.pickle')
-    airbus_models = pd.read_pickle('E:/Coding/spot_the_plane/app/static/dicts/airbusModels.pickle')
-    boeing_models = pd.read_pickle('E:/Coding/spot_the_plane/app/static/dicts/boeingModels.pickle')
-    planes_and_names = pd.read_pickle('E:/Coding/spot_the_plane/app/static/dicts/planes_and_names.pickle')
-    aircraft_list = random.sample(aircraft_list, len(aircraft_list))
-    quiz_list = random.sample(planes_and_names, len(planes_and_names))
-    quiz_counter = 0
-    current_plane = quiz_list[quiz_counter]
-    aircraft_true = current_plane[0]
-    aircraft_false = []
-    pic = current_plane[1]
-    for k, i in boeing_models.items():
-        if aircraft_true not in i:
-            aircraft_false.append(random.choice(i))
-
-    for k, i in airbus_models.items():
-        if aircraft_true not in i:
-            aircraft_false.append(random.choice(i))
-
-    for n in range(21):
-        a = aircraft_false.append(random.choice(aircraft_list))
-
-    for a in aircraft_false:
-        if a == aircraft_true:
-            aircraft_false.remove(a)
-    aircraft_false = random.sample(aircraft_false, 3)
-    choices = random.sample(aircraft_false + [aircraft_true], 4)
+    files = [os.path.basename(f) for f in glob.glob('app/static/images/*.jpg')]
+    filename = random.choice(files)
+    plane_true = filename.split('_')[0]
+    planes_false = random.sample(set([f.split('_')[0] for f in files if f not in [plane_true]]), 3)
+    choices = random.sample(planes_false + [plane_true], 4)
     answer_1 = choices[0]
     answer_2 = choices[1]
     answer_3 = choices[2]
     answer_4 = choices[3]
-    image = '/static/pics/{}'.format(pic)
+    image = '/static/images/{}'.format(filename)
         
     if request.method == 'POST':
         time.sleep(1)
         return redirect(url_for('quiz'))
     else:
         time.sleep(1)
-        return render_template('quiz.html', aircraft_true=aircraft_true, image=image, answer_1=answer_1, answer_2=answer_2, answer_3=answer_3, answer_4=answer_4)
+        return render_template('quiz.html', aircraft_true=plane_true, image=image, answer_1=answer_1, answer_2=answer_2, answer_3=answer_3, answer_4=answer_4)
