36f3be941312a3b70f3cbc72a0b9453d88fb4ec4
Jeff Nicholas <jefnic23@gmail.com>
2021-03-27 22:16:19 -0400

    convert game logic to js

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, jsonify
-import random, time, glob, os
+import random, glob, os
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,22 +9,8 @@ def index():
     else:
         return render_template('index.html')
 
-@app.route('/quiz', methods=['GET', 'POST'])
+@app.route('/quiz')
 def quiz():
     files = [os.path.basename(f) for f in glob.glob('app/static/images/*.jpg')]
-    filename = random.choice(files)
-    plane_true = filename.split('_')[0]
-    planes_false = random.sample(set([f.split('_')[0] for f in files if f not in [plane_true]]), 3)
-    choices = random.sample(planes_false + [plane_true], 4)
-    answer_1 = choices[0]
-    answer_2 = choices[1]
-    answer_3 = choices[2]
-    answer_4 = choices[3]
-    image = '/static/images/{}'.format(filename)
-        
-    if request.method == 'POST':
-        time.sleep(1)
-        return redirect(url_for('quiz'))
-    else:
-        time.sleep(1)
-        return render_template('quiz.html', aircraft_true=plane_true, image=image, answer_1=answer_1, answer_2=answer_2, answer_3=answer_3, answer_4=answer_4)
+    images = random.sample(files, len(files))
+    return render_template('quiz.html', images=images)
